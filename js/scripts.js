3e3ae23f690b88e6dde31079b4785117bf6f3d55
Pablo Barreyro and William Chu <pablo@>
2018-03-28 09:30:14 -0700

    add business logic to check if input begins with vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+// Business Logic
+var vowels = ["a","e","i","o","u"];
+
 function pigLatin(word) {
-  return word + "way";
+var firstLetter = word[0];
+console.log(firstLetter);
+var checkVowel;
+
+if (vowels.includes(firstLetter)) {
+  checkVowel = true;
+} else {
+  checkVowel = false;
 }
+console.log(checkVowel);
+
+  if (checkVowel === false) {
+    console.log("this isn't a vowel");
+  } else {
+  return word + "way";
+  }
+};
+
+
+// User Logic
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var userInput = $("input").val();
+    var translation = pigLatin(userInput);
+
+    $("#outputWord").text(translation);
+  });
+});
